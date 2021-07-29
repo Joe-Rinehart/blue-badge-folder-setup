729249becdcce552f91d738282ca08762b673ae4
Joe Rinehart <joerinehart91@gmail.com>
2021-07-29 15:44:14 -0400

    11.4

--- 05-Node-Server/server/controllers/usercontroller.js
+++ 05-Node-Server/server/controllers/usercontroller.js
@@ -4,22 +4,54 @@
 // let User = sequelize.import('../models/user.js')
 const router = require('express').Router()
 const User = require("../db").import("../models/user.js")
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs')
 
 router.post('/create', function(req,res) {
-
-    let userModel = {
+    User.create({
         email: req.body.user.email,
-        password: req.body.user.password,
-    };
-    User.create(userModel).then(function(user){
-        let responseObject = {
-            user: user
-        }
-        res.json(responseObject)
+        password: bcrypt.hashSync(req.body.user.password, 13)
+    })
+    .then (function createSuccess(user) {
+        let token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expiresIn: 60*60*24})
+        res.json({
+            user: user,
+            message: 'User successfully created!',
+            sessionToken: token
+        })
     })
     .catch(function(err) {
         res.status(500).json({error: err})
     })
+})
+
+router.post('/login', function(req,res) {
+    User.findOne({ 
+        where: { 
+            email: req.body.user.email
+        } 
+    })
+    .then(function loginSuccess(user){
+        if(user) {
+            bcrypt.compare(req.body.user.password, user.password, function(err, matches) {
+                if (matches) {
+            
+            let token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expiresIn: 60*60*24})
+            res.status(200).json({
+                user: user,
+                message:"user successfully logged in!",
+                sessionToken: token
+          })
+
+        } else {
+            res.status(502).send({error: "Login Failed"})
+        }
+        })
+    } else {
+         res.status(500).json({error: "User not found"})        
+    }
+})
+    .catch((err) => res.status(500).json({error: err}))
 })
 
 module.exports = router;
