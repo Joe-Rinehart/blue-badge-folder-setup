1bfc56d57de67ac70f61540b3b020892ee682cbe
Joe Rinehart <joerinehart91@gmail.com>
2021-08-02 14:50:48 -0400

    Server Modules 8/2/2021

--- 05-Node-Server/server/models/journal.js
+++ 05-Node-Server/server/models/journal.js
@@ -1,1 +1,22 @@
-module.exports = (sequelize, DataTypes) => {}
+module.exports = function (sequelize, DataTypes) {
+
+    const Journal = sequelize.define('journal', {
+
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        date: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        entry: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        owner: {
+            type: DataTypes.INTEGER
+        }
+    }) 
+    return Journal
+}
