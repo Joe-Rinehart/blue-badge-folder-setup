ae5d00960d847a0ec33cc13d152568f6a2d411ab
Joe Rinehart <joerinehart91@gmail.com>
2021-08-04 21:05:21 -0400

    modules 3.1

---
+++ 05-Node-Server/client/get-scripts.js
@@ -0,0 +1,20 @@
+/*
+Display by user
+*/
+function displayMine() {
+    console.log('displayMine Function Called')
+}
+
+/*
+Display All
+*/
+function displayAll() {
+    console.log('displayAll Function Called')
+}
+
+/*
+Display by title
+*/
+function displayByTitle() {
+    console.log('displayByTitle Function Called')
+}
---
+++ 05-Node-Server/client/post-edit-delete-scripts.js
@@ -0,0 +1,20 @@
+/*
+Post Journal
+*/
+function postJournal() {
+    console.log('postJournal Function Called')
+}
+
+/*
+Update Journal
+*/
+function editJournal(postId) {
+    console.log('editJournal Function Called')
+}
+
+/*
+Delete Journal
+*/
+function deleteJournal(postId) {
+    console.log('deleteJournal Function Called')
+}
