16de8f9739e3714177547ff87b549af73aa4c2e5
Joe Rinehart <joerinehart91@gmail.com>
2021-08-12 17:29:31 -0400

    Workout-logbook 8-12-2021

--- workout-logbook/server/controllers/usercontroller.js
+++ workout-logbook/server/controllers/usercontroller.js
@@ -1,7 +1,8 @@
-const router = require('express').Router()
-const User = require("../db").import("../models/user.js")
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs')
+const router = require("express").Router();
+const User = require("../db").import("../models/user");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
 
 router.post('/register', function(req,res) {
     User.create({
@@ -21,33 +22,33 @@ router.post('/register', function(req,res) {
     })
 })
 
-router.post('/login', function(req,res) {
-    User.findOne({ 
-        where: { 
-            username: req.body.username
-        } 
+router.post("/login", function (req, res) {
+    console.log(process.env.JWT_SECRET)
+    User.findOne({
+      where: {
+        username: req.body.username,
+      },
     })
-    .then(function loginSuccess(user){
-        if(user) {
-            bcrypt.compare(req.body.user.password, user.password, function(err, matches) {
-                if (matches) {
-            
-            let token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expiresIn: 60*60*24})
-            res.status(200).json({
+      .then(function loginSuccess(user) {
+        if (user) {
+          bcrypt.compare(req.body.password, user.passwordHash, function (err, matches) {
+            if (matches) {
+              let token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: 60 * 60 * 24 });
+              res.status(200).json({
                 user: user,
-                message:"user successfully logged in!",
-                sessionToken: token
-          })
-
+                message: "user successfully logged in!",
+                sessionToken: token,
+              });
+            } else {
+              res.status(502).json({ error: "Login Failed" });
+            }
+          });
         } else {
-            res.status(502).send({error: "Login Failed"})
+          res.status(500).json({ error: "User does not exist" });
         }
-        })
-    } else {
-         res.status(500).json({error: "User not found"})        
-    }
-})
-    .catch((err) => res.status(500).json({error: err}))
-})
+      })
+      .catch((err) => res.status(500).json({ error: err }));
+   });
+
 
 module.exports = router;
